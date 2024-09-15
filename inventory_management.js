4c8faeb37bd4e3d58955a6e64b3247fe70f027ca
Morgan Jacob <jacobmmori@gmail.com>
2024-09-15 17:32:12 -0400

    commenting

--- inventory_management.js
+++ inventory_management.js
@@ -39,8 +39,7 @@ console.log(`Stock status for ${inventory[0].name}: ${stockStatus}`);
 // Task 4- Create a Function to Check Low Stock Products
 
 function checkLowStockLevel(inventory) {
-    let lowStockItems = [];
-
+    
     inventory.forEach(item => {
         if (item.quantity <= item.lowStockLevel) {
             lowStockItems.push(`${item.name} is low in stock. Current quantity: ${item.quantity}`);
