ca6885d70f2d89f568aee31655f4bf4345a0d1d4
Morgan Jacob <jacobmmori@gmail.com>
2024-09-15 20:04:05 -0400

    Create processSale function

--- inventory_management.js
+++ inventory_management.js
@@ -12,13 +12,13 @@ let inventory = [
 
 function displayProductDetails (_name, price, quantity, lowStockLevel) {
     const status = quantity <= lowStockLevel ? "Low Stock" : "In Stock" //using ternary operator to determine stock status
-return `Product:${inventory.name}
+return `Product:${product.name}
         price: ${price}
         Quantity: ${quantity}       
         Low Stock Level: ${status}`}
 
         inventory.forEach(product => 
-            console.log(displayProductDetails(inventory.name, product.price, product.quantity, product.lowStockLevel)));
+            console.log(displayProductDetails(product.name, product.price, product.quantity, product.lowStockLevel)));
 
         
 
